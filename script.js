d69fcf55188204305cb80231d5b7a89879800cf8
Md FaizurRahman <114854264+Faizurcse@users.noreply.github.com>
2024-03-22 01:39:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+let arr = [1, 2, 3, 4]
+
+const p1 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+	const newArr = arr.filter((i)=>{
+			if(i%2!==0){
+				return i
+			}
+		})
+		  resolve(newArr)
+	},1000)
+})
+
+const p2 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+	const newArr1 = arr.filter((i)=>{
+			if(i%2===0){
+				return (i * 2)
+			}
+		})
+		  resolve(newArr1)
+	},2000)
+})
+
+p1.then((data)=>{
+	document.getElementById('output').innerText = data
+	return p2
+}).then((data1)=>{
+	document.getElementById('output').innerText = data1
+
+})
+
